249f82d9305dd0f977f54995f862674a2de76655
crespire <li.simmon@gmail.com>
2021-11-13 20:08:30 -0500

    Finish my_each

--- main.rb
+++ main.rb
@@ -1,5 +1,37 @@
 module Enumerable
   def my_each
-    # Stuff
+    return to_enum unless block_given?
+
+    for el in self do
+      yield el
+    end
   end
 end
+
+puts 'Testing #each'
+arr = [1, 2, 3, 4]
+arr2 = [12, 27, 1, 99, 12, 'a']
+arr3 = ['stesting', :are_we_sure, 'this', [5, 6], 'works', Random.new]
+
+puts 'Array 1'
+p arr.each
+p arr.my_each
+puts 'Testing #each block, output should be duplicated'
+arr.each { |item| p item }
+arr.my_each { |item| p item }
+
+puts '=================='
+puts 'Array 2'
+p arr2.each
+p arr2.my_each
+puts 'Testing #each block, output should be duplicated'
+p arr2.each { |item| p item }
+p arr2.my_each { |item| p item }
+
+puts '=================='
+puts 'Array 3'
+p arr3.each
+p arr3.my_each
+puts 'Testing #each block, output should be duplicated'
+arr3.each { |item| p item }
+arr3.my_each { |item| p item }
