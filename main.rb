edc2ef9e35b34e1cfe9893add7b70c2a92c1aa72
crespire <li.simmon@gmail.com>
2021-11-13 21:23:08 -0500

    Finish my_all?

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 module Enumerable
   def my_each
-    return to_enum unless block_given?
+    return to_enum(:my_each) unless block_given?
 
     length.times do |i|
       yield self[i]
@@ -10,7 +10,7 @@ module Enumerable
   end
 
   def my_each_with_index
-    return to_enum unless block_given?
+    return to_enum(:my_each_with_index) unless block_given?
 
     length.times do |i|
       yield self[i], i
@@ -20,8 +20,27 @@ module Enumerable
   end
 
   def my_select
-    return to_enum unless block_given?
+    return to_enum(:my_select) unless block_given?
 
     results = []
-  end 
+
+    my_each do |elem|
+      results.push(elem) if yield elem
+    end
+    results
+  end
+  
+  def my_all?(*pat)
+    if block_given?
+      my_each do |elem|
+        return false unless yield elem
+      end
+    else
+      my_each do |elem|
+        return false unless pat[0] === elem
+      end
+    end
+
+    true
+  end
 end
