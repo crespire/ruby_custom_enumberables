f7521529a744a9aa3f3376cacaa8ab447b90963e
crespire <li.simmon@gmail.com>
2021-11-13 20:09:02 -0500

    Move tests to separate file

---
+++ tests.rb
@@ -0,0 +1,27 @@
+puts 'Testing #each'
+arr = [1, 2, 3, 4]
+arr2 = [12, 27, 1, 99, 12, 'a']
+arr3 = ['testing', :are_we_sure, 'this', [5, 6], 'works', Random.new]
+
+puts 'Array 1'
+p arr.each
+p arr.my_each
+puts 'Testing #each block, output should be duplicated'
+arr.each { |item| p item }
+arr.my_each { |item| p item }
+
+puts '=================='
+puts 'Array 2'
+p arr2.each
+p arr2.my_each
+puts 'Testing #each block, output should be duplicated'
+p arr2.each { |item| p item }
+p arr2.my_each { |item| p item }
+
+puts '=================='
+puts 'Array 3'
+p arr3.each
+p arr3.my_each
+puts 'Testing #each block, output should be duplicated'
+arr3.each { |item| p item }
+arr3.my_each { |item| p item }
