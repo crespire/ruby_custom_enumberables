addeb1c4f09d731ae5df95f1a5b55378d31a1126
crespire <li.simmon@gmail.com>
2021-11-13 20:18:26 -0500

    Fix NameError

--- main.rb
+++ main.rb
@@ -2,18 +2,16 @@ module Enumerable
   def my_each
     return to_enum unless block_given?
 
-    for el in self do
-      yield el
+    length.times do |i|
+      yield self[i]
     end
   end
 
   def my_each_with_index
     return to_enum unless block_given?
 
-    index = 0
-    length.times do i
-      yield self[i], index
-      index += 1
+    length.times do |i|
+      yield self[i], i
     end
   end
 end
