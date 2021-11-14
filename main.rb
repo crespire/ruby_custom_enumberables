e76aaf8a1a51788942564dfe83684cb7d2b94276
crespire <li.simmon@gmail.com>
2021-11-13 22:53:47 -0500

    Change single line blocks from do/end to {}

--- main.rb
+++ main.rb
@@ -2,20 +2,14 @@ module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
 
-    length.times do |i|
-      yield self[i]
-    end
-
+    length.times { |i| yield self[i] }
     self
   end
 
   def my_each_with_index(*args)
     return to_enum(:my_each_with_index, *args) unless block_given?
 
-    length.times do |i|
-      yield self[i], i
-    end
-
+    length.times { |i| yield self[i], i }
     self
   end
 
@@ -23,58 +17,38 @@ module Enumerable
     return to_enum(:my_select) unless block_given?
 
     results = []
-
-    my_each do |elem|
-      results.push(elem) if yield elem
-    end
+    my_each { |elem| results.push(elem) if yield elem }
     results
   end
 
   def my_all?(pattern = nil)
     if block_given?
-      my_each do |elem|
-        return false unless yield elem
-      end
+      my_each { |elem| return false unless yield elem }
     else
-      my_each do |elem|
-        return false unless pattern === elem
-      end
+      my_each { |elem| return false unless pattern === elem }
     end
-
     true
   end
 
   def my_any?(pattern = nil)
     if block_given?
-      my_each do |elem|
-        return true if yield elem
-      end
+      my_each { |elem| return true if yield elem }
     else
-      my_each do |elem|
-        return true if pattern === elem
-      end
+      my_each { |elem| return true if pattern === elem }
     end
-
     false
   end
 
   def my_none?(pattern = nil)
     if block_given?
-      my_each do |elem|
-        return false if yield elem
-      end
+      my_each { |elem| return false if yield elem }
     else
       unless pattern.nil? # Pattern provided
-        my_each do |elem|
-          return false if pattern === elem
-        end
+        my_each { |elem| return false if pattern === elem }
       else
-        my_each do |elem|
-          return false if false ^ elem
-        end
+        my_each { |elem| return false if false ^ elem }
       end
     end
-
     true
   end
 end
