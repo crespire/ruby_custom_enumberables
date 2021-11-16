067ba4fafb6f2dced0d064d9f48fb7f0c88e3482
crespire <li.simmon@gmail.com>
2021-11-16 12:27:40 -0500

    Remove pry-byebug

--- main.rb
+++ main.rb
@@ -1,5 +1,3 @@
-require 'pry-byebug'
-
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
@@ -81,8 +79,6 @@ module Enumerable
     end
 
     memo = initial || first
-
-    #binding.pry
 
     if block_given?
       my_each_with_index do |ele, i|
